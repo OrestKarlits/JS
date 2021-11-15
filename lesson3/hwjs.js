e0aaf136cc12c959389254552173415086ad23c3
OrestKarlits <orestkarlits@gmail.com>
2021-11-16 00:54:18 +0200

    homework, lesson3

---
+++ lesson3/hwjs.js
@@ -0,0 +1,91 @@
+// --створити масив з:
+//     - з 5 числових значень
+// - з 5 стічкових значень
+// - з 5 значень стрічкового, числового та булевого типу
+// - та вивести його в консоль
+
+// let number = [10, 69, 8, 47, 91];
+// let str = ['asd', 'bcbc', 'asja', 'ashha', 'quu'];
+// let xxx = ['agss', 25, true, 7, false]
+// console.log(number);
+// console.log(str);
+// console.log(xxx);
+
+
+// Створити пустий масив. Наповнити його будь-якими значеннями звертаючись до конкретного індексу. Вивести в консоль
+
+// let xxx = [];
+// xxx[0] = true;
+// xxx[1] = 123;
+// xxx[2] = 'fsahaghsah';
+// xxx[3] = false;
+// xxx[4] = 3265464546;
+// xxx[5] = 'asbachjabcabj';
+// console.log(xxx);
+
+
+
+
+// За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+
+// for (let i = 0; i < 10; i++) {
+//     document.write(`<div>adasd</div>`)
+// }
+
+
+// За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+// for (let i = 0; i < 10; i++) {
+//     document.write(`<div>${i} safasccaca</div>`)
+// }
+
+
+
+// За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+
+
+// let xxx = 0;
+// while (xxx<20) {
+//     document.write(`<h1>sdasdasda</h1>`)
+//     xxx++;
+// }
+
+
+
+// За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині
+
+
+// let xxx = 0;
+// while (xxx<20) {
+//     document.write(`<h1>sdasdasda ${xxx}</h1>`)
+//     xxx++;
+// }
+
+
+
+// Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+
+
+
+// let xxx = [15, 41, 59, 14, 13, 16, 98, 45, 698, 79]
+// for (let abc = 0; abc < xxx.length; abc++) {
+//     console.log(xxx[abc])
+// }
+
+
+// Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі
+
+// let x = ['sahsg', 'joji', 'gcsagfxc', 'mskamal', 'oaksodka', 'lkasmoa', 'asaxa', 'fatfztFT', 'aysgxyasgyagx', 'asdahdbh']
+// for (let sss = 0; sss < x.length; sss++) {
+//     console.log(x[sss])
+// }
+
+
+// Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+
+
+let p = [123, true, 'fsdsfsd', 566, 'qweqqqqqqqq', 456, false, 'fdksofksofk', 445, 'fasfafafcasfdvmd']
+for (let lll = 0; lll < p.length; lll++) {
+    console.log(p[lll])
+}
