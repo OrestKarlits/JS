f5f38b9d5bcb4f0e8ba1b9f09631cf5863e14add
OrestKarlits <orestkarlits@gmail.com>
2021-12-02 23:19:29 +0200

    Homework, lesson8

---
+++ lesson8/hw8.js
@@ -0,0 +1,57 @@
+// Напишіть код,  який за допомоги document.getElementById або
+// document.getElementsByClassName або document.getElementsByTagName :
+
+
+// -- отримує текст з параграфа з id "content"
+
+// let getp = document.getElementById('content');
+// console.log(getp.innerText);
+
+
+// отримує текст з блоку з id "rules"
+
+// let prules = document.getElementById('rules');
+// console.log(prules.innerText);
+
+
+// замініть текст параграфа з id 'content' на будь-який інший
+
+// let xxx = document.getElementById('content').innerText = 'szdxfgcvhbvab';
+// console.log(xxx);
+
+
+// замініть текст параграфа з id 'rules' на будь-який інший
+
+// let xxx = document.getElementById('rules').innerText = 'ascacdnajcnascnaicnak';
+// console.log(xxx);
+
+
+// змініть кожному елементу колір фону на червоний
+
+// let children_red = document.body.children;
+// for (const childrenRedElement of children_red) {
+//     childrenRedElement.style.background = 'red';
+// }
+
+
+// змініть кожному елементу колір тексту на синій
+
+// let children_color_blue = document.body.children;
+// for (const childrenColorBlueElement of children_color_blue) {
+//     childrenColorBlueElement.style.color = 'blue';
+// }
+
+
+// отримати весь список класів елемента з id=rules і вивести їх в console.log
+
+// let listrules = document.getElementById('rules');
+// console.log(listrules.classList);
+
+
+// поміняти колір тексту у всіх елементів fc_rules на червоний
+
+// let colorrules = document.getElementsByClassName('fc_rules');
+// for (const colorFcRule of colorrules) {
+//     colorFcRule.style.color = 'red';
+// }
+
